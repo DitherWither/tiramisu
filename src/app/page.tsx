00d56f1979cffe5c532149d5b77d319b946c3104
Vardhan Patil <hi@vardhanpatil.com>
2024-03-09 20:15:50 +0530

    Add login button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,31 @@
-"use client";
-import Link from "next/link";
-import { useState } from "react";
+import { LoginButton } from "~/components/auth/login-button";
 import { Button } from "~/components/ui/button";
 import { ModeToggle } from "~/components/ui/mode-toggle";
-import { H1, P, TableBody, TableHead, TableHeader, Table, TableCell } from "~/components/ui/typography";
+import {
+  H1,
+  P,
+  TableBody,
+  TableHead,
+  TableHeader,
+  Table,
+  TableCell,
+} from "~/components/ui/typography";
+import { getServerAuthSession } from "~/server/auth";
 
-export default function HomePage() {
-  const [count, setCount] = useState(0);
+export default async function HomePage() {
+  const session = await getServerAuthSession();
   return (
     <main>
-      <H1>Hallo</H1>
-      <ModeToggle />
-      <P>{count}</P>
-      <Table>
-        <TableHead>
-          <TableHeader>Id</TableHeader>
-          <TableHeader>Name</TableHeader>
-        </TableHead>
-        <TableBody>
-          <TableCell>1</TableCell>
-          <TableCell>Vardhan</TableCell>
-        </TableBody>
-      </Table>
-      <Button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        Click Me
-      </Button>
+      <H1>Home</H1>
+      {session ? (
+        <>
+          <P>Logged in as {session.user.email}</P>
+        </>
+      ) : (
+        <>
+          <LoginButton />
+        </>
+      )}
     </main>
   );
 }
