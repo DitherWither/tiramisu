fa1027749e55b806cac5df88b868a94863c45e3a
Vardhan Patil <hi@vardhanpatil.com>
2024-03-09 23:49:36 +0530

    Create UI for patient creation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,15 @@
-import { LoginButton } from "~/components/auth/login-button";
-import { Button } from "~/components/ui/button";
-import { ModeToggle } from "~/components/ui/mode-toggle";
 import {
   H1,
-  P,
-  TableBody,
-  TableHead,
-  TableHeader,
-  Table,
-  TableCell,
-} from "~/components/ui/typography";
+  P
+} from "@ui";
 import { getServerAuthSession } from "~/server/auth";
 
 export default async function HomePage() {
   const session = await getServerAuthSession();
   return (
-    <main>
+    <>
       <H1>Home</H1>
       <P>Logged in as {session?.user.email}</P>
-    </main>
+    </>
   );
 }
---
+++ src/app/patients/page.tsx
@@ -0,0 +1,10 @@
+import { H1 } from "@ui";
+import { NewPatientButton } from "~/components/patient-form";
+
+export default function PatientsPage() {
+  return (
+    <>
+      <H1>Patients</H1>
+    </>
+  );
+}
